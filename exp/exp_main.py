76a3f02cb1f502b709ea72c1c461e10291cece7c
Michalis Charatzoglou <micharatz97@gmail.com>
2023-10-29 09:30:53 +0200

    init experiments

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -185,7 +185,7 @@ class Exp_Main(Exp_Basic):
         test_data, test_loader = self._get_data(flag='test')
         if test:
             print('loading model')
-            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
+            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + 'checkpoint.pth')))
 
         preds = []
         trues = []
